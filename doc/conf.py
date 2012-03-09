f8103016fae93de692b32789a81c186e293ee692
Jason Furtney <jkfurtney@gmail.com>
2012-03-08 18:53:04 -0600

    Adding mock object for c module for readthedocs.org

--- doc/conf.py
+++ doc/conf.py
@@ -244,3 +244,24 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['cfmm']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
