292a7533cc80ac18b0b1b68fd75c1be02fa04445
Jason Furtney <jkfurtney@gmail.com>
2012-03-24 15:41:29 -0500

    changes for readthedocs.org

--- doc/conf.py
+++ doc/conf.py
@@ -11,6 +11,28 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+# mock out the c extension (cfmm) so we can build on RTD
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['cfmm']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
 import sys, os
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -245,23 +267,3 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
 
-
-class Mock(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(self, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            return type(name, (), {})
-        else:
-            return Mock()
-
-MOCK_MODULES = ['cfmm']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
