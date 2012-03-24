6ae39e710910b5e2984445dbace5ec3da455c167
Jason Furtney <jkfurtney@gmail.com>
2012-03-24 15:51:54 -0500

    More RTD changes

--- doc/conf.py
+++ doc/conf.py
@@ -12,6 +12,8 @@
 # serve to show the default.
 
 # mock out the c extension (cfmm) so we can build on RTD
+import sys, os
+
 class Mock(object):
     def __init__(self, *args, **kwargs):
         pass
@@ -33,12 +35,10 @@ for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
 
-import sys, os
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
 
