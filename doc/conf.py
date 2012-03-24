f5a08b642052340e1b8a6a400a9569530fdbdaee
Jason Furtney <jkfurtney@gmail.com>
2012-03-24 16:27:27 -0500

    adding numpy mock for RTD

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['cfmm']
+MOCK_MODULES = ['cfmm', 'numpy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
