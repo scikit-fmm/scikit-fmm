e1863b14eab51e671313cae8db161a0c2a246146
Jason Furtney <jkfurtney@gmail.com>
2012-06-03 15:25:01 -0500

    Fixed a 32/64bit bug. Things should work on 64bit now. Two tests are still not passing.
    From the numpy api: "A Python int is equivalent to a C long"
    
    Thanks to Luigi Costalli and Daniel Wheeler for pointing this out.

--- skfmm/fmm.cpp
+++ skfmm/fmm.cpp
@@ -86,7 +86,7 @@ static PyObject *distance_method(PyObject *self, PyObject *args)
     return NULL;
   }
 
-  flag = (PyArrayObject *)PyArray_FROMANY(pflag, PyArray_INT, 1,
+  flag = (PyArrayObject *)PyArray_FROMANY(pflag, PyArray_LONG, 1,
                                           10, NPY_IN_ARRAY);
   if (!flag)
   {
