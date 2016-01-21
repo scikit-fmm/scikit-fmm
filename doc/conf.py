cb2d192eb6a87f368b2daac2d962c43ed0da65b8
Jason Furtney <jkfurtney@gmail.com>
2016-01-21 14:20:52 -0600

    updating CONTRIBUTORS.txt and fixing the mocks for read the docs

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['cfmm', 'numpy', 'pheap']
+MOCK_MODULES = ['skfmm.cfmm', 'numpy', 'skfmm.pheap']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
