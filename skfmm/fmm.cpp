5709473de015a7e0b1a8517c1e67f3dbb61507cc
Vivien Holmes <vivien.holmes@postgrad.manchester.ac.uk>
2026-01-21 14:59:36 +0000

    allow casting of drivers

--- skfmm/fmm.cpp
+++ skfmm/fmm.cpp
@@ -202,7 +202,7 @@ static PyObject* distance_method(PyObject* self, PyObject* args)
       return nullptr;
     }
 
-    drivers = (PyArrayObject *)PyArray_FROMANY(pdrivers, NPY_UINT, 1, 12, NPY_IN_ARRAY);
+    drivers = (PyArrayObject *)PyArray_FROMANY(pdrivers, NPY_UINT, 1, 12, NPY_IN_ARRAY_IN_ARRAY | NPY_ARRAY_FORCECAST);
 
     std::printf("pdrivers = %x\n", pdrivers);                                          
     std::printf("drivers = %x\n", drivers);                                          
